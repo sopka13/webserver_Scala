59223e9b42c67c8a2a4a6acf2fdd8c66eb730b90
sopka13 <sopka13@mail.ru>
2022-01-20 14:48:57 +0300

    first commit

---
+++ build.sbt
@@ -0,0 +1,5 @@
+name := "webServer"
+
+version := "0.1"
+
+scalaVersion := "2.13.8"
---
+++ src/main/scala/Main.scala
@@ -0,0 +1,13 @@
+/*
+	This is webserver for support websites
+
+	Work targets:
+	1. Add miltiplex i/o for sockets
+	2. Add the project to the git repository
+ */
+
+object Main {
+	def main(args: Array[String]): Unit = {
+
+	}
+}
